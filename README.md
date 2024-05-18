b244ae2c82206b5525c201ef178e1e4d1cfc3edb
Paul Liljenberg <paul@bane.enterprises>
2024-05-18 21:44:48 +0200

    rename

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Requires root.
 
 ```
-curl -sfL https://raw.github.com/oxypwn/nixos-config/main/install.sh > install.sh; bash -x install.sh
+curl -sfL -H 'Pragma: no-cache' -H 'Cache-Control: no-cache, no-store' https://raw.github.com/oxypwn/nixos-config/main/install.sh > install.sh; bash -x install.sh
 ```
--- _lib/install.sh
+++ _lib/install.sh
@@ -8,7 +8,7 @@ _defaultvalue FILESYSTEM _lib/preinstall/filesystem
 _defaultvalue INSTALL_DRIVE /dev/sda
 _defaultvalue NIXOS _lib/preinstall/nixos
 _defaultvalue FSTAB _lib/preinstall/fstab
-_defaultvalue CHROOT _lib/chrootplz
+_defaultvalue CHROOTPLZ _lib/chrootplz
 
 
 [ ! -f "${0}" ] && echo "Don't run this directly from curl. Save to file first." && exit
@@ -22,7 +22,7 @@ _loadblock "${FSTAB}"
 _filesystem_fstab
 _loadblock "${NIXOS}"
 _nixos_pre
-_loadblock "${CHROOT}"
+_loadblock "${CHROOTPLZ}"
 _chroot_postscript
 fi
 
