81b6a27b1f379a170e2a1de5acd835664e91f6f8
Paul Liljenberg <paul@bane.enterprises>
2024-05-18 10:11:10 +0200

    add sda

--- _lib/install.sh
+++ _lib/install.sh
@@ -2,6 +2,7 @@
 
 _defaultvalue FILESYSTEM_TYPE ext4
 _defaultvalue FILESYSTEM pre/filesystem
+_defaultvalue INSTALL_DRIVE /dev/sda
 
 [ ! -f "${0}" ] && echo "Don't run this directly from curl. Save to file first." && exit
 [ -e "${POSTSCRIPT}" ] && INCHROOT=true || INCHROOT=false
