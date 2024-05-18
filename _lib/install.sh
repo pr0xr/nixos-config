49334631b8cc723acdf68c79c1206c1750d80629
Paul Liljenberg <paul@bane.enterprises>
2024-05-18 10:05:53 +0200

    add default value

--- _lib/install.sh
+++ _lib/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 _defaultvalue FILESYSTEM_TYPE ext4
+_defaultvalue FILESYSTEM pre/filesystem
 
 [ ! -f "${0}" ] && echo "Don't run this directly from curl. Save to file first." && exit
 [ -e "${POSTSCRIPT}" ] && INCHROOT=true || INCHROOT=false
