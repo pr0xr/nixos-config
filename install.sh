5f28249ccaa5f4db4d641c01134335f34006e6c4
Paul Liljenberg <paul@bane.enterprises>
2024-05-18 13:50:51 +0200

    update path

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-REMOTE=https://raw.github.com/oxypwn/nixos-config/master/
+REMOTE=https://raw.githubusercontent.com/oxypwn/nixos-config/main
 . <(curl -fsL "${REMOTE}/_lib/functions.sh"); _loadblock "_lib/install"
