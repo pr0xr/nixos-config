3a79011cdafbbf40e85ab873aed73dcb971bbcb2
Paul Liljenberg <paul@bane.enterprises>
2024-05-19 18:08:48 +0200

    wip

--- _lib/preinstall/nixos.sh
+++ _lib/preinstall/nixos.sh
@@ -20,5 +20,5 @@ _nixos_pre () {
 _nixos_post () {
     cd $HOME \
         && mkdir /nixos-config && bash -c 'curl -LSs https://api.github.com/repos/oxypwn/nixos-config/tarball | tar xvzf - -C /nixos-config --strip-components=1'
-    bash -c 'NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1 nixos-rebuild switch --flake \"/nixos-config#${NIXNAME}\"'
+    NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1 nixos-rebuild switch --flake "/nixos-config#${NIXNAME}"
 }
