87f488b6d6ff940dc4a42e549e86b455b4a65c3f
Paul Liljenberg <paul@bane.enterprises>
2024-05-20 14:39:04 +0200

    updates

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -32,7 +32,7 @@ in {
     "mupdf-1.17.0"
   ];
 
-  #boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot/EFI";
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
