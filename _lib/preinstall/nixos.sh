0dd7898fa1ae3a44d3bea562695a613ae4ec2405
Paul Liljenberg <paul@bane.enterprises>
2024-05-22 00:25:25 +0200

    check for existing config dir

--- _lib/preinstall/nixos.sh
+++ _lib/preinstall/nixos.sh
@@ -18,16 +18,18 @@ _nixos_pre () {
 }
 
 _nixos_post () {
-    #mkdir -p /boot/EFI/{systemd,BOOT}
-    mkdir -p /nixos-config
-    if [ $GITHUB_TOKEN ]
+    if [ ! -d "/nixos-config" ]
     then
-        curl \
-            -H "Accept: application/vnd.github.raw+json" \
-            -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-            -LSs https://api.github.com/repos/oxypwn/nixos-config/tarball | tar xvzf - -C /nixos-config --strip-components=1
-    else
-        curl -LSs https://api.github.com/repos/oxypwn/nixos-config/tarball | tar xvzf - -C /nixos-config --strip-components=1
+        mkdir -p /nixos-config
+        if [ $GITHUB_TOKEN ]
+        then
+            curl \
+                -H "Accept: application/vnd.github.raw+json" \
+                -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+                -LSs https://api.github.com/repos/oxypwn/nixos-config/tarball | tar xvzf - -C /nixos-config --strip-components=1
+        else
+            curl -LSs https://api.github.com/repos/oxypwn/nixos-config/tarball | tar xvzf - -C /nixos-config --strip-components=1
+        fi
     fi
     NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1 nixos-rebuild switch --flake "/nixos-config#${NIXNAME}" --show-trace
 }
