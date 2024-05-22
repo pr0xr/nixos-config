bbfb056ed83413060db9374727602f6807ba4c45
Paul Liljenberg <paul@bane.enterprises>
2024-05-22 12:38:23 +0200

    updates

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -76,7 +76,7 @@ in {
   # setup windowing environment
   services.xserver = if linuxGnome then {
     enable = true;
-    layout = "us";
+    layout = "se,dvorak_a5";
     desktopManager.gnome.enable = true;
     displayManager.gdm.enable = true;
   } else {
