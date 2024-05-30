dae492aa223e3a911047f9b5383a67d525dece76
Paul Liljenberg <paul@bangofbits.com>
2024-05-31 01:13:18 +0200

    remove

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -132,13 +132,7 @@ in {
     niv
     rxvt_unicode
     xclip
-    (pkgs.buildFHSUserEnv {
-        name = "fhs";
-        runScript = "fish";
-        targetPkgs = pkgs: with pkgs; [
-            nixpkgs-unstable.rye
-        ]; 
-    })
+
     # For hypervisors that support auto-resizing, this script forces it.
     # I've noticed not everyone listens to the udev events so this is a hack.
     (writeShellScriptBin "xrandr-auto" ''
